a6cfe7c717918726cce1ef301a52a85a61d630fd
Shiva Kant Sharma <shivakantsharma33@gmail.com>
2023-10-25 13:35:12 +0530

    v1 code for date parameter

--- src/main/java/com/avananta/models/BaseModel.java
+++ src/main/java/com/avananta/models/BaseModel.java
@@ -1,14 +1,14 @@
 package com.avananta.models;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
@@ -17,14 +17,18 @@ import java.util.Date;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @CreatedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date createdAt;
 
+    @LastModifiedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date updatedAt;
 
     private boolean isDelete;
